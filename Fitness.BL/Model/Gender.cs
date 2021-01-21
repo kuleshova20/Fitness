2ad197e297fb27051097e574d8240a7eb7085488
Ann <kuleshowaaaaaaa@gmail.com>
2021-01-21 16:18:14 +0300

    Add project files.

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace Fitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name of gender can't be empty or null.", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness.BL/Model/User.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+
+namespace Fitness.BL.Model
+{
+    public class User
+    {
+        #region Свойства
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gender.
+        /// </summary>
+        public Gender Gender { get; }
+
+        /// <summary>
+        /// BirthDate.
+        /// </summary>
+        public DateTime BirthDate { get; }
+
+        /// <summary>
+        /// Weight.
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Height.
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name"> Name. </param>
+        /// <param name="gender"> Gender. </param>
+        /// <param name="birthDate"> BirthDate. </param>
+        /// <param name="weight"> Weight. </param>
+        /// <param name="height"> Height. </param>
+        public User(string name, 
+                    Gender gender, 
+                    DateTime birthDate, 
+                    double weight, 
+                    double height)
+        {
+            #region Checking conditions
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name of user can't be empty or null.", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Gender can't be empty or null.", nameof(gender));
+            }
+
+            if(birthDate < DateTime.Parse("01.01.1900. ") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentException("Impossible birthDate.", nameof(birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Weight can't be less or equal of null.", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentException("Height can't be less or equal of null.", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+
+        }
+      
+        public override string ToString()
+        {
+            return Name;
+        }
+
+
+    }
+}
